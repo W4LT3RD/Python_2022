f12a505f74187fe388e35e58b46c7bf002877eea
Walter Davila <3004903990101@ingenieria.usac.edu.gt>
2022-03-06 02:01:41 -0600

    Comentarios 1

--- OpenCV/Hola.py
+++ OpenCV/Hola.py
@@ -11,8 +11,12 @@ Run
   git config --global user.email "you@example.com"
   git config --global user.name "Your Name"
 Para cada carpeta:
-    git init (solo una vez)
-    git status -s
-    git add nombre.py <-agregar solo un archivo
-    git add . <-(guardar todo)
-"""
+    git init                        <-solo una vez por cada carpeta nueva
+    git status -s                   <-verificar si hay cambios
+    git add nombre.py               <-agregar solo un archivo
+    git add .                       <-guardar todo
+    git commit -m "Comentarios"     <-guardar un punto de cambio de código
+    git log --oneline               <-ver los puntos de guardado
+    git reset -- hard ###           <-para regresar a un punto de guardado, ### número de identificación
+    
+     """
