34abcf51cda3bbf6b905e4089a31479ccbc93139
Walter Davila <3004903990101@ingenieria.usac.edu.gt>
2022-03-06 01:54:51 -0600

    Comentarios

--- OpenCV/Hola.py
+++ OpenCV/Hola.py
@@ -5,4 +5,14 @@ print("------------\n|Hola mundo|\n------------")
 #Comentario 
 """
 Comentario xd
+Para Git:
+Run
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+Para cada carpeta:
+    git init (solo una vez)
+    git status -s
+    git add nombre.py <-agregar solo un archivo
+    git add . <-(guardar todo)
 """
